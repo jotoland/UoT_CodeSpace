a52c3567ce72208297ee7a5709f26149d874d9ec
rioneal <richard@Richards-MacBook-Pro.local>
2017-04-13 15:21:15 -0500

    Basic Boss functionality
    basic boss functionality, spawns, moves into view, and fires at user,
    added new boss weapon sounds

---
+++ uot/Assets/StopByBoundary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopByBoundary : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) 
+	{
+		GameObject boss = GameObject.FindGameObjectWithTag("Lvl05Boss");
+		if(other.tag == ("Lvl05Boss"))
+			{
+			Debug.Log ("Print");
+			boss.GetComponent<Mover> ().enabled = false;
+			boss.GetComponent<Rigidbody> ().velocity = transform.forward * 0;
+			boss.GetComponent<EvasiveManeuver>().enabled = true;
+			boss.GetComponent<WeaponController>().enabled = true;
+
+			}
+	}
+}
