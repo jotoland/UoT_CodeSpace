d2f10736ddfb0737eae553bbe5c8b62602e56478
coZmoKramer <jtoland3.14@gmail.com>
2017-04-14 20:33:22 -0500

    Integration Testing
    1. ShotSpawn pickups
    2. oneUp pickups
    3. missile pickups
    4. rupee pickups
    5. Points test
    6. Life lost test
    7. rupee spawn test

---
+++ uot/Assets/_TestingScripts/IntegrationTesting/MisslePickUpTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MisslePickUpTest : MonoBehaviour {
+	GameController gc;
+
+	void Start(){
+		gc = GameObject.Find ("GameController").GetComponent <GameController> ();
+		gc.setTest ();
+		gc.clearValues ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (gc.getMissleCount () == 1) {
+			IntegrationTest.Pass (this.gameObject);
+			Debug.Log (IntegrationTest.passMessage);
+		}else {
+			Debug.Log (IntegrationTest.failMessage);
+		}
+		outComeInt (1, gc.getMissleCount ());
+		gc.clearValues ();
+	}
+
+	public void outComeString(string expected, string actual){
+		Debug.Log ("Expected: " + expected + " Actual: " + actual);
+	}
+
+	public void outComeInt(int expected, int actual){
+		Debug.Log ("Expected: " + expected + " Actual: " + actual);
+	}
+}
---
+++ uot/Assets/_TestingScripts/IntegrationTesting/ShotSpawnCollectedTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotSpawnCollectedTest : MonoBehaviour {
+	PlayerController pc;
+
+	void Start(){
+		pc = GameObject.FindGameObjectWithTag("Player").GetComponent <PlayerController> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (pc.numberOfSpawns == 3) {
+			IntegrationTest.Pass (this.gameObject);
+			Debug.Log (IntegrationTest.passMessage);
+
+		}else {
+			Debug.Log (IntegrationTest.failMessage);
+		}
+		outComeInt (3, pc.numberOfSpawns);
+	}
+
+	public void outComeString(string expected, string actual){
+		Debug.Log ("Expected: " + expected + " Actual: " + actual);
+	}
+
+	public void outComeInt(int expected, int actual){
+		Debug.Log ("Expected: " + expected + " Actual: " + actual);
+	}
+}
